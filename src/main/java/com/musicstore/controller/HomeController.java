bad32e646d492e6f43e0e821666620e8a96f6649
imad <imad.live1990@gmail.com>
2020-05-11 06:12:13 +0200

    develop add product function and save it in the database

--- src/main/java/com/musicstore/controller/HomeController.java
+++ src/main/java/com/musicstore/controller/HomeController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.musicstore.dao.IProductDao;
 import com.musicstore.model.Product;
 
@@ -34,5 +37,37 @@ public class HomeController {
 		Product product= productDao.getProductById(productId);
 		model.addAttribute(product);
 		return "viewProduct";
+	}
+	
+	@RequestMapping("/admin")
+	public String adminPage() {
+		return "admin";
+	}
+	
+	@RequestMapping("/admin/productInventory")
+	public String productInventory(Model model){
+		List<Product> products = productDao.getAllProduct();
+		model.addAttribute("products", products);
+		return "productInventory";
+		
+	}
+	
+	
+	@RequestMapping("/admin/productInventory/addProduct")
+	public String addProduct(Model model){
+		Product product = new Product();
+		product.setProductCategory("instrument");
+		product.setProductCondition("new");
+		product.setProductStatus("active");
+		
+		model.addAttribute("product", product);
+		
+		return "addProduct";
+	}
+	
+	@RequestMapping(value ="/admin/productInventory/addProduct", method = RequestMethod.POST)
+	public String addProductPost(@ModelAttribute("product") Product product) {
+		productDao.addProduct(product);
+		return "redirect:/admin/productInventory";
 	}
 }
